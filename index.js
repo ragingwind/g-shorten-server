bf6aa281d90caa71f4dd68c26c7e19fe1f352e9a
ragingwind <ragingwind@gmail.com>
2015-06-13 17:49:06 -0700

    Fetch shortenurl with got

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 'use strict';
 
 var express = require('express');
+var got = require('got');
 var app = express();
 var port = process.env.PORT;
-
-console.log(port);
+var url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + process.env.APIKEY
 
 app.get('/shorten/', function (req, res) {
-    res.send('shortened');
+  var opts = {
+    body: JSON.stringify({longUrl: req.query.longurl}),
+    headers: {
+      'content-type': 'application/json'
+    },
+    strictSSL: true,
+    json: true
+  };
+
+  got.post(url, opts, function(err, data) {
+    if (err) {
+      res.status(400).end(data);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 var server = app.listen(port, function() {
